b9b55556e2c27fe761a867caedbbbf66d338bd71
jai 3 <47060357+jdadonut@users.noreply.github.com>
2021-07-05 18:29:17 -0400

    fixed thing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Plugin } = require('powercord/entities');
 const Timezones = require("./util/timezones.js");
 
-module.exports = class Template extends Plugin {
+module.exports = class TimeIn extends Plugin {
     startPlugin() {
         powercord.api.commands.registerCommand({
             command: "time-in",
@@ -15,7 +15,7 @@ module.exports = class Template extends Plugin {
                         result: `It is currently ${Date.now().toLocaleString("default", {timeZone: timezone})} in ${args[0]}`};
             },
             autocomplete: (args) => {
-                if (args[0] == void 0 || args[0] == undefined){
+                if (args[0] == void 0 || args[0] == undefined || args[0] == ""){
                     return {commands: Timezones, header: "Timezones"}
                 }
                 return {commands: Array.from(Timezones).filter(x => x.toLowerCase().includes(args[0].toLowerCase())),
