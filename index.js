e707c6a1df9f021f76e9381af93ca2103bb22f93
jai 3 <47060357+jdadonut@users.noreply.github.com>
2021-07-05 18:23:47 -0400

    fixed thing

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const { Plugin } = require('powercord/entities');
-
+const {api: {commands: {registerCommand, unregisterCommand}}} = powercord;
+const Timezones = require("util/timezones.js");
+const timezones = require('./util/timezones');
 module.exports = class Template extends Plugin {
     startPlugin() {
-        return;
+        registerCommand({
+            command: "time-in",
+            description: "Shows you the time in a specified timezone",
+            usage: "{c} <timezone>",
+            executor: (args) => {
+                let timezone = Array.from(Timezones).filter(x => x.toLowerCase() === args[0].toLowerCase())[0];
+                if (timezone === undefined)
+                    return {send: false, result: `${args[0]} is not a valid timezone!`};
+                return {send: false, 
+                        result: `It is currently ${Date.now().toLocaleString("default", {timeZone: timezone})} in ${args[0]}`};
+            },
+            autocomplete: (args) => {
+                if (args[0] == void 0 || args[0] == undefined){
+                    return {commands: Timezones, header: "Timezones"}
+                }
+                return {commands: Array.from(timezones).filter(x => x.toLowerCase().includes(args[0].toLowerCase())),
+                        header: "Timezones"};
+                
+                
+            }
+
+
+
+        })
     }
     pluginWillUnload() {
         return;
